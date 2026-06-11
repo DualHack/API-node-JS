cfef7de682312b026684fcb3b6494ccb99f95fb4
manuel.luis <mpl31052001@gmail.com>
2026-06-11 14:59:02 +0100

    Feat: improve code formatting and enhance API health check logging

--- src/server.js
+++ src/server.js
@@ -5,11 +5,25 @@ const connectDatabase = require('./config/db');
 const PORT = process.env.PORT || 4000;
 
 const startServer = async () => {
-  await connectDatabase();
+    await connectDatabase();
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 };
 
 startServer();
+
+setInterval(() => {
+    console.log('Performing periodic cleanup of old reports...');
+    fetch("https://angosecapi.onrender.com/api/health", {
+        method: "GET",
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("API Health Check:", data);
+        })
+        .catch((error) => {
+            console.error("API Health Check Failed:", error);
+        });
+},14 * 1000); 
